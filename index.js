f1e044903c6845d368f7ca63f6c4b3f55144b0d0
F.Gonçalves <boriscasoy123@gmail.com>
2024-03-12 09:36:26 -0300

    f

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
-
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
-
 
 app.listen(port, () => {
     console.log(`Servidor funcionando na porta:`, port)
